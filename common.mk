cd5ef7e76ed156b8477d5f2834ecacb23f5476bb
Albert Tang <tangalbert919@yahoo.com>
2022-06-09 19:15:01 -0500

    sm8450-common: Set PRODUCT_SHIPPING_API_LEVEL to 31
    Change-Id: If2a65a3f362f8fab9d501c1edad564e8b9a3724b

--- common.mk
+++ common.mk
@@ -377,7 +377,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_taro/android.hardware.sensor.stepdetector.xml
 
 # Shipping API
-PRODUCT_SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
