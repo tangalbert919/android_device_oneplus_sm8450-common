e7e3dc65737f27440e32321d285f70acb9b0f0b4
Albert Tang <tangalbert919@yahoo.com>
2022-08-07 13:52:00 -0500

    sm8450-common: Add OnePlus 10T support
    Change-Id: I9fa453d4b20c589e9a1a9bdba3ebe5696a9da784

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter negroni,$(TARGET_DEVICE)),)
+ifneq ($(filter negroni ovaltine,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "negroni"
+write_headers "negroni ovaltine"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
