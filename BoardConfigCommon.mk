f721adf188a31bd0d1a77f1d15f2bd9b3e21b45a
Albert Tang <tangalbert919@yahoo.com>
2022-09-19 11:59:26 -0500

    Revert "sm8350-common: Switch to legacy sepolicy_vndr"
    This is unnecessary for sm8450.
    
    This reverts commit 5030e4164cee51e27787bc985bb121dfd4a9ffa5.
    
    Change-Id: Ic4f3421cea5f26515f3ecb481429797d5dc3b993

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,7 +173,7 @@ BOOT_SECURITY_PATCH := 2022-10-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Verified Boot
